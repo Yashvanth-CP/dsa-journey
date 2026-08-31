6c448ac452adad207a50a2e7667715c7e41c96c0
Yashvanth CP <yashvanthcp01@gmail.com>
2026-08-31 20:08:44 +0530

    min and max problem done

--- Arrays/2058_FInd_nim_max.cpp
+++ Arrays/2058_FInd_nim_max.cpp
@@ -2,44 +2,97 @@
 Given a linked list head, return an array of length 2 containing [minDistance, maxDistance] where minDistance is the minimum distance between any two distinct critical points and maxDistance is the maximum distance between any two distinct critical points. If there are fewer than two critical points, return [-1, -1].
 */
 
-
 #include <iostream>
 #include <vector>
+#include <climits>
 #include <algorithm>
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int maxWater = 0;
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> ans = {-1, -1};
 
-        while (left < right) {
-            int width = right - left;
-            int h = min(height[left], height[right]);
-
-            int area = width * h;
-            maxWater = max(maxWater, area);
-
-            if (height[left] < height[right])
-                left++;
-            else
-                right--;
+        if (head == nullptr || 
+            head->next == nullptr || 
+            head->next->next == nullptr) {
+            return ans;
         }
 
-        return maxWater;
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+
+        int index = 1;
+        int firstCritical = -1;
+        int lastCritical = -1;
+        int minDistance = INT_MAX;
+
+        while (curr->next != nullptr) {
+
+            bool isCritical =
+                (curr->val > prev->val && curr->val > curr->next->val) ||
+                (curr->val < prev->val && curr->val < curr->next->val);
+
+            if (isCritical) {
+
+                if (firstCritical == -1) {
+                    firstCritical = index;
+                } 
+                else {
+                    minDistance = min(
+                        minDistance,
+                        index - lastCritical
+                    );
+                }
+
+                lastCritical = index;
+            }
+
+            prev = curr;
+            curr = curr->next;
+            index++;
+        }
+
+        if (firstCritical == lastCritical) {
+            return ans;
+        }
+
+        int maxDistance = lastCritical - firstCritical;
+
+        return {minDistance, maxDistance};
     }
 };
 
 int main() {
+
+    // Create linked list:
+    // 5 -> 3 -> 1 -> 2 -> 5 -> 1 -> 2
+
+    ListNode* head = new ListNode(5);
+    head->next = new ListNode(3);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(2);
+    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next = new ListNode(1);
+    head->next->next->next->next->next->next = new ListNode(2);
+
     Solution sol;
 
-    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    vector<int> result =
+        sol.nodesBetweenCriticalPoints(head);
 
-    int result = sol.maxArea(height);
-
-    cout << "Maximum water: " << result << endl;
+    cout << "Minimum distance: " << result[0] << endl;
+    cout << "Maximum distance: " << result[1] << endl;
 
     return 0;
 }
